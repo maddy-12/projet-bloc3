d4b449e6cec8cf9cb0067b95f339effb97115b23
Madina <benaferi-madina@hotmail.fr>
2021-12-02 23:21:31 +0100

    Create first verison of HTML

--- views/home.php
+++ views/home.php
@@ -1,16 +1,45 @@
-<?php 
-include ('views/header.php');
-?>
-<!-- Cartes de recettes -->
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container">
+  <div class="name-slogan">
+    <h1>BIOCOOK</h1>
+    <span>Votre santé et votre appétit sont notre priorité</span>
   </div>
-</div>
 
-<?php
-include ('views/footer.php');
-?>
+  <div class="row d-flex justify-content-center">
+    <div class="col-3">
+      <!-- Cartes de recettes -->
+      <div class="card" style="width: 18rem;">
+        <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-card ">Voir la recette</a>
+        </div>
+      </div>
+    </div>
+   <!-- à supprimer  lorsqu'on fait la boucle-->
+    <div class="col-3">
+      <!-- Cartes de recettes -->
+      <div class="card" style="width: 18rem;">
+        <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-card ">Voir la recette</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-3">
+      <!-- Cartes de recettes -->
+      <div class="card" style="width: 18rem;">
+        <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary btn-card ">Voir la recette</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div> 
