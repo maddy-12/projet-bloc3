277ed9c6655a96449e6d9c8558e5f45ccf55132e
eloodieb <eloodie.bouget@gmail.com>
2021-12-15 22:17:29 +0100

    Update link and recipe card

--- views/favorites.php
+++ views/favorites.php
@@ -2,7 +2,7 @@
 include('header.php');
 ?>
 
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,95 +15,85 @@ include('header.php');
 
     <!-- CSS -->
     <link rel="stylesheet" href="../assets/style.css">
-    
-    <title>BIOCOOK</title>
-  </head>
-  <body>
 
-<?php 
-include ('header.php');
-?>
-  <main class="container">
+    <title>BIOCOOK</title>
+</head>
+
+<body>
+
+    <?php
+    include('header.php');
+    ?>
+    <main class="container">
         <div class="row mg-top">
             <div class="col-12 text-center">
                 <h2 class="text-uppercase">Mes recettes favorites</h2>
             </div>
         </div>
-     
-    <div class="row my-5">
-        <div class="col-6"> 
-            <div class="row">
-                <div class="col-6">
-                    <img class="rounded float-end img-fluid" src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" alt="">
-                </div>
-                <div class="col-6 my-2">
-                    <h3 class="h4">Titre recette</h3>
-                    <ul class="list-group">
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Voir la recette</a></li>
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Liste de courses</a></li>
-                    <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Supprimer</a></li>
-                    </ul>
-                </div>
-            </div>      
-        </div>
-        <div class="col-6"> 
-            <div class="row">
-                <div class="col-6">
-                    <img class="rounded float-end img-fluid" src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" alt="">
-                </div>
-                <div class="col-6  my-2">
-                    <h3 class="h4">Titre recette</h3>
-                    <ul class="list-group">
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Voir la recette</a></li>
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Liste de courses</a></li>
-                    <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Supprimer</a></li>
-                    </ul>
-                </div>
-            </div>      
-        </div>
-    </div>
-    <div class="row my-5">
-        <div class="col-6"> 
-            <div class="row">
-                <div class="col-6">
-                    <img class="rounded float-end img-fluid" src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" alt="">
-                </div>
-                <div class="col-6  my-2">
-                    <h3 class="h4">Titre recette</h3>
-                    <ul class="list-group">
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Voir la recette</a></li>
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Liste de courses</a></li>
-                    <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Supprimer</a></li>
-                    </ul>
-                </div>
-            </div>      
-        </div>
-        <div class="col-6"> 
-            <div class="row">
-                <div class="col-6">
-                    <img class="rounded float-end img-fluid" src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" alt="">
-                </div>
-                <div class="col-6  my-2">
-                    <h3 class="h4">Titre recette</h3>
-                    <ul class="list-group">
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Voir la recette</a></li>
-                        <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Liste de courses</a></li>
-                    <li class="list-inline-item my-2"><a class="btn btn-card" href="#" role="button">Supprimer</a></li>
-                    </ul>
-                </div>
-            </div>      
-        </div>
-    </div>
 
+        <div class="row d-flex justify-content-center my-4">
+            <div class="col-5">
+                <!-- Recette favorite -->
+                <div class="card">
+                    <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <a href="recipeDetail.php" class="btn btn-card ">Voir la recette </a>
+                        <a href="shoppingList.php" class="btn btn-card ">Liste de courses</a>
+                        <button type="button" class="btn btn-card">Supprimer</button>
+                    </div>
+                </div>
+            </div>
 
-</main>
-<?php 
-include ('footer.php');
-?>
+            <div class="col-5">
+                <!-- Recette favorite -->
+                <div class="card">
+                    <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <a href="recipeDetail.php" class="btn btn-card ">Voir la recette </a>
+                        <a href="shoppingList.php" class="btn btn-card ">Liste de courses</a>
+                        <button type="button" class="btn btn-card">Supprimer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center my-4">
+            <div class="col-5">
+                <!-- Recette favorite -->
+                <div class="card">
+                    <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <a href="recipeDetail.php" class="btn btn-card ">Voir la recette </a>
+                        <a href="shoppingList.php" class="btn btn-card ">Liste de courses</a>
+                        <button type="button" class="btn btn-card">Supprimer</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-5">
+                <!-- Recette favorite -->
+                <div class="card">
+                    <img src="https://www.atelierdeschefs.com/media/recette-d13804-bibimbap.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <a href="recipeDetail.php" class="btn btn-card ">Voir la recette </a>
+                        <a href="shoppingList.php" class="btn btn-card ">Liste de courses</a>
+                        <button type="button" class="btn btn-card">Supprimer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <?php
+    include('footer.php');
+    ?>
 </body>
-  
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </html>
-
-
