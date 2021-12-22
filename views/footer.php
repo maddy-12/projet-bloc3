17f0a0be6fdd643fda8b6883c84eb89c8f55ce2d
Elodie BOUGET <elodie.bouget@viacesi.fr>
2021-12-22 16:19:12 +0100

    Update footer

--- views/footer.php
+++ views/footer.php
@@ -18,18 +18,15 @@
   </head>
   <div class="container-fluid bg-footer">
     <footer class="py-3 my-4">
-      <div class="row my-3 text-center">
-        <div class="col-4 text-light"><p>LOGO</p></div>
-        <div class="col-4 text-light">
-          <div class="row">
-            <div class="col-6 text-end">
-            <p>Nous suivre :</p>
-            </div>
-            <div class="col-6 text-start">
-            <i class="fab fa-facebook-f me-2"></i>
-            <i class="fab fa-instagram"></i>
-            </div>
-          </div>
+      <div class="row my-3 text-center align-items-center">
+        <div class="col-4 text-light"><img src="../assets/img/logoWithBackground.png" alt="Logo BIOCOOK" class="rounded"></div>
+        <div class="col-4 text-light d-flex justify-content-center">
+        
+            <p class="me-2" >Nous suivre :</p>
+            <a href="" class="link-light"> <i class="fab fa-facebook-f me-4"></i></a>
+            <a href="" class="link-light"> <i class="fab fa-instagram"></i></a>
+           
+       
         </div>
         <div class="col-4"><a class="btn btn-light" href="views/contact.php" role="button">Contact</a></div>
       </div>
