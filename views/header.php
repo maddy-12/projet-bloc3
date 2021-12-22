c6364654a2969d36c88673709035d6e32a723400
Elodie BOUGET <elodie.bouget@viacesi.fr>
2021-12-22 12:20:53 +0100

    Add Logo

--- views/header.php
+++ views/header.php
@@ -22,7 +22,7 @@
 </head>
 <nav id="navbar-white" class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="../index.php">BIOCOOK</a>
+    <a class="navbar-brand" href="../index.php"><img src="../assets/img/logo.png" alt="Logo BIOCOOK"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
