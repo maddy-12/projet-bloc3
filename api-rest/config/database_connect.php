d34d319c396bc0a4ec5f195a20ae0d677ac62cd3
Madina <benaferi-madina@hotmail.fr>
2022-01-14 12:09:53 +0100

    connexion BDD

---
+++ api-rest/config/database_connect.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database
+{
+    // Connexion à la base de données
+    private $host = "localhost";
+    private $db_name = "biocook";
+    private $username = "root";
+    private $password = "";
+    public $connexion;
+
+
+    //getter pour la connexion
+    public function getConnection()
+    {
+        $this->connexion = null;
+        try {
+            $this->connexion =
+                new PDO("mysql:host=" . $this->host . $this->db_name, $this->username, $this->password);
+            $this->connexion->exec('SET NAMES "UTF8"');
+        } catch (PDOException $exception) {
+            echo "Erreur de connexion : " . $exception->getmessage();
+        }
+        return $this->connexion;
+    }
+}
