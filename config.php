9e62224b1646c7c1b01bb12e174151cc089815c8
Madina <benaferi-madina@hotmail.fr>
2022-01-14 11:10:54 +0100

    config

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+//DB Params
+define('DB_HOST', 'localhost'); // BDD host
+define('DB_USER', 'root');//BDD root
+define('DB_PASS','');//BDD MDP
+define('DB_NAME','biocook');//NOM DE LA BDD
+
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT (Liens dynamiques)
+define('URLROOT', 'http://localhost/biocook');
+
+//Nom du site
+define('SITENAME','BIOCOOK');
