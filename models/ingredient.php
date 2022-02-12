1a143d6373212c4557e67d9d10fb7412ab424a38
Madina <benaferi-madina@hotmail.fr>
2022-02-12 14:04:17 +0100

    comment ingredients

--- models/ingredient.php
+++ models/ingredient.php
@@ -1,11 +1,10 @@
 <?php
 
-include ("api-rest/config/database_connect.php");
+// include ("api-rest/config/database_connect.php");
 
- function GetAllIngredientsFromRecipe($id_recipe) {
-    global $connexion;
-    $response = $connexion->query("SELECT ingredient.name, ingredient_recipe.quantity, unit.text FROM ingredient INNER JOIN ingredient_recipe on ingredient.id = ingredient_recipe.id_ingredient
-    INNER JOIN unit on ingredient_recipe.id_unit = unit.id where ingredient_recipe.id_recipe = '$id_recipe'");
-    return $response->fetchAll();
-}
-
+//  function GetAllIngredientsFromRecipe($id_recipe) {
+//     global $connexion;
+//     $response = $connexion->query("SELECT ingredient.name, ingredient_recipe.quantity, unit.text FROM ingredient INNER JOIN ingredient_recipe on ingredient.id = ingredient_recipe.id_ingredient
+//     INNER JOIN unit on ingredient_recipe.id_unit = unit.id where ingredient_recipe.id_recipe = '$id_recipe'");
+//     return $response->fetchAll();
+// }
