fbc18804c982cfed960032b6d6e00443efd45a32
eloodieb <eloodie.bouget@gmail.com>
2022-02-18 10:30:19 +0100

    Add react project / Update space admin

---
+++ reactbiocook/Helpers/recipeData.js
@@ -0,0 +1,24 @@
+// Helpers/filmsData.js
+
+const data = [
+    {
+        id: 1,
+        title: "Crêpe",
+        category: "Dessert",
+        overview: "De bonnes crêpes miam miam",
+        cooking_time: "3",
+        preparing_time: "12",
+        date: "2017-12-13"
+    },
+    {
+        id: 2,
+        title: "Pancake",
+        category: "Dessert",
+        overview: "De bons pancakes hummm miam miam",
+        cooking_time: "3",
+        preparing_time: "12",
+        date: "2017-12-13"
+    }
+]
+
+export default data
